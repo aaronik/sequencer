e3c97b374ad58c4083fededea82da9a28ece5469
Aaron Sullivan <aasullivan1618@gmail.com>
2022-11-09 12:20:16 -0800

    Got neighbor classnames all worked out

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,31 @@ function Grid() {
       for (let j = 0; j < GRID_SIZE; j++) {
         const classNames = []
         classNames.push('grid-item')
-        classNames.push(`row-${i} column-${j}`)
+        // classNames.push(`row-${i} column-${j}`)
+
+        // Assign immediate neighbor classNames
+        for (let m = i - 1; m <= i + 1; m++) {
+          for (let n = j - 1; n <= j + 1; n ++) {
+            // we don't want to label ourselves as a neighbor to ourselves
+            if (m === i && n === j) continue
+            classNames.push(`${m}-${n}-neighbor`)
+          }
+        }
+
+        // Assign secondary neighbor classNames
+        for (let m = i - 2; m <= i + 2; m++) {
+          for (let n = j - 2; n <= j + 2; n ++) {
+            // we don't want to label ourselves as a neighbor to ourselves
+            if (m === i && n === j) continue
+            classNames.push(`${m}-${n}-second-neighbor`)
+          }
+        }
+
 
         const key = `${i}-${j}`
 
         gridItems.push(
-          <div key={key} className={classNames.join(' ')}>{}</div>
+          <div key={key} className={classNames.join(' ')}>{key}</div>
         )
       }
     }
