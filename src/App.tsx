7e8caabf32a4dd997761778b72647a07cb013c6e
Aaron Sullivan <aasullivan1618@gmail.com>
2022-11-09 14:23:02 -0800

    Grid animations are working great
    Now we just need:
    
    * Make the sound happen
    * Allow for multiple grids

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 
 const GRID_SIZE = 16
 const PROPAGATION_SPEED = 50
+const PLAYBACK_SPEED = 200 // per column
 
 const buildItemClass = (i: number, j: number) => `item-${i}-${j}`
 const buildNeighborClass = (i: number, j: number) => `neighbor-${i}-${j}`
 const buildSecondNeighborClass = (i: number, j: number) => `second-neighbor-${i}-${j}`
+const buildColumnClass = (column: number) => `column-${column}`
 
 const animate = (i: number, j: number) => {
-  // TODO For some speed savings here, we could keep track of these in JS instead of reaching into the dom
   const item = document.querySelector('.' + buildItemClass(i, j))
   const itemNeighbors = document.querySelectorAll('.' + buildNeighborClass(i, j))
   const itemSecondNeighbors = document.querySelectorAll('.' + buildSecondNeighborClass(i, j))
@@ -25,7 +26,6 @@ const animate = (i: number, j: number) => {
 
 function Grid() {
   const onClick = (i: number, j: number) => () => {
-    animate(i, j) // TODO move this to on play
     document.querySelector('.' + buildItemClass(i, j))?.classList.toggle('enabled')
   }
 
@@ -36,7 +36,7 @@ function Grid() {
       for (let j = 0; j < GRID_SIZE; j++) {
         const classNames = []
         classNames.push('grid-item')
-        classNames.push(`column-${j}`)
+        classNames.push(buildColumnClass(j))
         classNames.push(buildItemClass(i, j))
 
         // Assign secondary neighbor classNames
@@ -95,13 +95,53 @@ function PlayButton({ isPlaying, onClick }: PlayButtonProps) {
   )
 }
 
+const removeAllActiveColumns = () => {
+  const allItems = document.querySelectorAll('.grid-item')
+  allItems.forEach(item => item.classList.remove('active-column'))
+}
+
+const playColumn = (column: number) => {
+  removeAllActiveColumns()
+
+  const playingItems = document.querySelectorAll('.' + buildColumnClass(column))
+  playingItems.forEach((item, row) => {
+    item.classList.add('active-column')
+    if (item.classList.contains('enabled')) {
+      animate(row, column)
+      // sound(row, column)
+    }
+  })
+}
+
 function App() {
   const [isPlaying, setIsPlaying] = useState(false)
+  const playbackInterval = useRef<ReturnType<typeof setInterval> | null>(null)
+
+  const startPlay = () => {
+    let column = 1
+    playColumn(0)
+    playbackInterval.current = setInterval(() => {
+      playColumn(column)
+      column = (column + 1) % (GRID_SIZE)
+    }, PLAYBACK_SPEED)
+  }
+
+  const stopPlay = () => {
+    clearInterval(playbackInterval.current!)
+    removeAllActiveColumns()
+  }
+
+
+  const togglePlay = () => {
+    if (isPlaying) stopPlay()
+    else startPlay()
+    setIsPlaying(!isPlaying)
+  }
 
   return (
     <div className="App">
       <Grid />
-      <PlayButton isPlaying={isPlaying} onClick={() => setIsPlaying(!isPlaying)} />
+      <PlayButton isPlaying={isPlaying} onClick={togglePlay} />
     </div>
   );
 }
