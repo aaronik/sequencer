710c3cf70fbfe1a52ed8d010f852157366ce66cc
Aaron Sullivan <aasullivan1618@gmail.com>
2022-12-08 14:09:06 -0800

    Make more efficient by removing grid calculations from every render
    Grid doesn't need to run this each time.
    
    I discovered this exploring useMemo, and achieved the same thing with
    useMemo(() => ..., []). Then I realized any useMemo with an empty array
    can just as easily be put outside of the function body. So that's what I
    did.

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -6,64 +6,63 @@ type GridProps = {
   activeColor: string
 }
 
-export default function Grid({ activeColor }: GridProps) {
-  const onClick = (i: number, j: number) => () => {
-    toggleGridItem(i, j)
-  }
+const generateInnerGrid = () => {
+  const gridItems = []
 
-  const generateInnerGrid = () => {
-    const gridItems = []
+  for (let i = 0; i < GRID_SIZE; i++) {
+    for (let j = 0; j < GRID_SIZE; j++) {
+      const classNames = []
+      classNames.push('grid-item')
+      classNames.push(buildColumnClass(j))
+      classNames.push(buildItemClass(i, j))
+      // // This is for development. Uncomment this to get an upward trending file from left to right
+      // if (i + j === GRID_SIZE - 1) classNames.push('enabled')
 
-    for (let i = 0; i < GRID_SIZE; i++) {
-      for (let j = 0; j < GRID_SIZE; j++) {
-        const classNames = []
-        classNames.push('grid-item')
-        classNames.push(buildColumnClass(j))
-        classNames.push(buildItemClass(i, j))
-        // // This is for development. Uncomment this to get an upward trending file from left to right
-        // if (i + j === GRID_SIZE - 1) classNames.push('enabled')
+      // Assign secondary neighbor classNames
+      for (let m = i - 2; m <= i + 2; m++) {
+        for (let n = j - 2; n <= j + 2; n++) {
+          // we don't want to label ourselves as a neighbor to ourselves
+          if (m === i && n === j) continue
 
-        // Assign secondary neighbor classNames
-        for (let m = i - 2; m <= i + 2; m++) {
-          for (let n = j - 2; n <= j + 2; n++) {
-            // we don't want to label ourselves as a neighbor to ourselves
-            if (m === i && n === j) continue
-
-            classNames.push(buildSecondNeighborClass(m, n))
-          }
+          classNames.push(buildSecondNeighborClass(m, n))
         }
-
-        // Assign immediate neighbor classNames
-        for (let m = i - 1; m <= i + 1; m++) {
-          for (let n = j - 1; n <= j + 1; n++) {
-            // we don't want to label ourselves as a neighbor to ourselves
-            if (m === i && n === j) continue
-            classNames.push(buildNeighborClass(m, n))
-
-            // Second neighbor classes were added to all around, so here we remove them.
-            const index = classNames.findIndex(cn => cn === buildSecondNeighborClass(m, n))
-            classNames.splice(index, 1)
-          }
-        }
-
-        const key = `${i}-${j}`
-
-        // The data attributes here are needed for both CSS animations and serialization
-        gridItems.push(
-          <div data-i={i} data-j={j} key={key} className={classNames.join(' ')} onClick={onClick(i, j)}>{}</div>
-        )
       }
-    }
 
-    return gridItems
+      // Assign immediate neighbor classNames
+      for (let m = i - 1; m <= i + 1; m++) {
+        for (let n = j - 1; n <= j + 1; n++) {
+          // we don't want to label ourselves as a neighbor to ourselves
+          if (m === i && n === j) continue
+          classNames.push(buildNeighborClass(m, n))
+
+          // Second neighbor classes were added to all around, so here we remove them.
+          const index = classNames.findIndex(cn => cn === buildSecondNeighborClass(m, n))
+          classNames.splice(index, 1)
+        }
+      }
+
+      const key = `${i}-${j}`
+
+      // The data attributes here are needed for both CSS animations and serialization
+      gridItems.push(
+        <div data-i={i} data-j={j} key={key} className={classNames.join(' ')} onClick={() => toggleGridItem(i, j)}>{}</div>
+      )
+    }
   }
+
+  return gridItems
+}
+
+const innerGrid = generateInnerGrid()
+
+export default function Grid({ activeColor }: GridProps) {
 
   return (
     <div className="grid">
       <style>
         {`.grid div.grid-item.enabled { background-color: ${activeColor};`}
       </style>
-      {generateInnerGrid()}
+      {innerGrid}
     </div>
   )
 }
